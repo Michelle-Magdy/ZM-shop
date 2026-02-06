e8ac07b86a007a7f2dfd0d2ae6b2951534aefd08
Zyad Mohamed Ahmed <zm857891@gmail.com>
2026-02-07 00:50:33 +0200

    Fixed like button style

--- frontend/app/components/home/LikeButton.jsx
+++ frontend/app/components/home/LikeButton.jsx
@@ -13,7 +13,7 @@ export default function LikeButton() {
       " bg-white rounded-full w-7 h-7 shadow-lg flex justify-center items-center p-1 hover:cursor-pointer absolute top-2 right-2 transition-all duration-300 ease-in-out";
   } else {
     cssClass =
-      " bg-primary rounded-full w-7 h-7 shadow-lg flex justify-center items-center p-1 hover:cursor-pointer absolute top-2 right-2 lg:-right-10 transition-all duration-300 ease-in-out group-hover:right-2";
+      "bg-white dark:bg-primary rounded-full w-7 h-7 shadow-lg flex justify-center items-center p-1 hover:cursor-pointer absolute top-2 right-2 lg:-right-10 transition-all duration-300 ease-in-out group-hover:right-2";
   }
   return (
     <button className={cssClass} onClick={toggleLike}>
