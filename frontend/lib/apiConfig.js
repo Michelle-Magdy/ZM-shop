841c751533e96fad8e97f570cbe0d89a5bd6aeda
Zyad Mohamed Ahmed <zm857891@gmail.com>
2026-04-21 14:22:26 +0200

    Frontend public images url

--- frontend/lib/apiConfig.js
+++ frontend/lib/apiConfig.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/api/v1`;
-export const IMAGES_BASE_URL =
-  process.env.NEXT_PUBLIC_IMAGES_URL ||
+export const IMAGES_BASE_URL = process.env.NEXT_PUBLIC_ENV === "development" ? 
+  process.env.NEXT_PUBLIC_IMAGES_URL :
   `${process.env.NEXT_PUBLIC_API_URL}/images`;
