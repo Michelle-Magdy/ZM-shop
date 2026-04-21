8c494c70ff7e9e89367f27eab0d0df9358f8258f
Michelle <michel01225474756@gmail.com>
2026-04-21 23:11:34 +0200

    edit cookies on server api

--- frontend/lib/api/serverApi.js
+++ frontend/lib/api/serverApi.js
@@ -7,18 +7,12 @@ export default async function serverApiClient() {
       ? `${process.env.NEXT_PUBLIC_API_URL}/api/v1`
       : `${process.env.NEXT_PUBLIC_LOCAL_URL}/api/v1`;
   //used with server components fetching to forward cookies to node server
-  const cookieStore = cookies();
-
-  const cookieHeader = cookieStore
-    .getAll()
-    .map((c) => `${c.name}=${c.value}`)
-    .join("; ");
-
+  const cookieStore = await cookies();
   return axios.create({
     baseURL: url,
     headers: {
       "Content-Type": "application/json",
-      Cookie: cookieHeader,
+      Cookie: cookieStore.toString(),
     },
   });
 }
