6e49341234cb5a4f7548fe6e9fa6e33c2aac6ef1
Michelle <michel01225474756@gmail.com>
2026-04-30 02:26:48 +0300

    fix: cookies

--- backend/util/jwt.js
+++ backend/util/jwt.js
@@ -17,9 +17,12 @@ export const createTokenAndSetCookie = (user, res, rememberMe = false) => {
     expires: new Date(Date.now() + expiresInDays * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", 
-    maxAge: expiresInDays * 24 * 60 * 60 * 1000, 
-    // domain: process.env.NODE_ENV === "production" ? ".railway.app" : undefined,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: expiresInDays * 24 * 60 * 60 * 1000,
+    domain:
+      process.env.NODE_ENV === "production"
+        ? "https://zm-shop-production.up.railway.app"
+        : undefined,
     path: "/",
   };
 
