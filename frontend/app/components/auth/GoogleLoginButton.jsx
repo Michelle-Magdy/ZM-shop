300f3f5422f964625c237744909faf93f76cb8f2
Zyad Mohamed Ahmed <zm857891@gmail.com>
2026-05-02 21:19:41 +0300

    Admin google login fix

--- frontend/app/components/auth/GoogleLoginButton.jsx
+++ frontend/app/components/auth/GoogleLoginButton.jsx
@@ -5,37 +5,41 @@ import { GoogleLogin } from "@react-oauth/google";
 import toast from "react-hot-toast";
 
 export default function GoogleLoginButton() {
-  const handleSuccess = async (credentialResponse) => {
-    try {
-      // credentialResponse.credential is the Google ID token
-      const res = await apiClient.post("/auth/google/token", {
-        token: credentialResponse.credential,
-      });
+    const handleSuccess = async (credentialResponse) => {
+        try {
+            // credentialResponse.credential is the Google ID token
+            const res = await apiClient.post("/auth/google/token", {
+                token: credentialResponse.credential,
+            });
 
-      console.log("Login successful!", res.data);
+            console.log("Login successful!", res.data);
 
-      window.location.href = "/";
-    } catch (error) {
-      console.error("Login failed:", error);
-      alert(
-        "Login failed: " + (error.response?.data?.message || "Unknown error"),
-      );
-    }
-  };
+            if (res.data.user.roles.includes("admin"))
+                window.location.href = "/admin";
+            else window.location.href = "/";
+            window.location.href = "/";
+        } catch (error) {
+            console.error("Login failed:", error);
+            alert(
+                "Login failed: " +
+                    (error.response?.data?.message || "Unknown error"),
+            );
+        }
+    };
 
-  const handleError = () => {
-    console.log("Login Failed");
-    toast.error("error during login try again");
-  };
+    const handleError = () => {
+        console.log("Login Failed");
+        toast.error("error during login try again");
+    };
 
-  return (
-    <div className="w-fit ">
-      <GoogleLogin onSuccess={handleSuccess} onError={handleError} />
+    return (
+        <div className="w-fit ">
+            <GoogleLogin onSuccess={handleSuccess} onError={handleError} />
 
-      {/* <GoogleLogin
+            {/* <GoogleLogin
         onSuccess={(res) => console.log(res)}
         onError={() => console.log("error")}
       /> */}
-    </div>
-  );
+        </div>
+    );
 }
