3cd2330c47f567de1cc0c8cac21740141bb2721e
Zyad Mohamed Ahmed <zm857891@gmail.com>
2026-05-02 19:29:42 +0300

    app.js updated

--- backend/app.js
+++ backend/app.js
@@ -57,7 +57,7 @@ const corsOptions = {
       return callback(null, true);
     }
 
-    return callback(new Error(`CORS blocked for origin: ${origin}`));
+    return callback(null, false)
   },
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   credentials: true,
@@ -72,7 +72,7 @@ app.use((req, res, next) => {
 
 app.use(cors(corsOptions));
 // Handle preflight
-app.options("/*splat", cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json({ limit: "10kb" }));
 app.use(cookieParser());
